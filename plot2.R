cf5333a53fd112777a5eb413f60a0d9e0d4897a9
Rogowiec <Wojciech.Rogowiec@sanofi.com>
2021-07-08 17:33:05 +0200

    Plot 1

--- plot2.R
+++ plot2.R
@@ -15,11 +15,27 @@ dataloc <- paste(getwd(), "/household_power_consumption.txt", sep= "")
 
 introdata <- fread(dataloc, sep=";")
 
+head(introdata)
+
 introdata$Global_active_power <- as.double(introdata$Global_active_power)
 
-introdata$Date <- as.Date(introdata$Date, "%d/%m/%Y")
+introdata <- subset(introdata, Date == "2007/02-01" | Date == "2007-02-02")
 
-introdata <- subset(introdata, Date == "2007-02-01" | Date == "2007-02-02")
+introdata$Date <- as.Date(introdata$Date, "%Y/%m/%d")
+
+introdata$Date <- paste(introdata$Date, introdata$Time)
+
+
+
+introdata$Date <- strptime(introdata$Date, format = "%Y/%m/%d %H:%M:%S")
+
+introdata$Time <- strptime(introdata$Time, "%H:%M:%OS")
+
+introdata$Time <- as.POSIXct(introdata$Time,format="%H:%M:%S")
+
+class(introdata$Date)
+
+plot(introdata$Date, introdata$Global_active_power)
 
 par(mar = c(6,10,4,10))
-hist(introdata$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main =  "Global Active Power")
+
